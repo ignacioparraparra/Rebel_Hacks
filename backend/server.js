b3bcee6bfc009435b86746a71afa27afa1ebb60c
Ignacio Parra <ignacioparra57@gmail.com>
2026-02-20 13:05:40 -0800

    added base routes

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const app = express()
 
+const authRoutes = require('./routes/auth.routes.js')
+const studentRoutes = require('./routes/students.routes.js')
+const teacherRoutes = require('./routes.teachers.routes.js')
+
+app.use('/auth', authRoutes)
+app.use('/student', studentRoutes)
+app.use('/teacher', teacherRoutes)
+
 app.listen(7777, (err) => {
     if (err) return console.log('server failed')
     console.log("Server listening on port 7777")
