8490c443c5065d7ff2bcb762a5c94718459153bb
Ry Suriyathep <rysu986@gmail.com>
2026-02-20 22:10:52 -0800

    favicon updated

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -8,12 +8,12 @@ const navClass = ({ isActive }) => (isActive ? "nav-link active" : "nav-link");
 function Navbar() {
   return (
     <nav className="navbar">
-      <NavLink to="/dashboard" className={navClass} end><h1 className="nav-link-element element-home">scholarChips</h1></NavLink>
+      <NavLink to="/" className={navClass} end><h1 className="nav-link-element element-home">scholarChips</h1></NavLink>
 
       <div className="navbar-links">
         <NavLink to="/dashboard" className={navClass} end><div className="nav-link-element">Dashboard</div></NavLink>
         <NavLink to="/prizes" className={navClass}><div className="nav-link-element">Prizes</div></NavLink>
-        <NavLink to="/classes" className={navClass}><div className="nav-link-element">Classes</div></NavLink>
+        {/* <NavLink to="/classes" className={navClass}><div className="nav-link-element">Classes</div></NavLink> */}
         <NavLink to="/leaderboard" className={navClass}><div className="nav-link-element">Leaderboard</div></NavLink>
 
       </div>
