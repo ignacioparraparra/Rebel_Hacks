3f42c2e7e4b06c56bb6e02cd2ff674b6aaa42bd1
Andrew <drewnguyen2006@gmail.com>
2026-02-20 16:29:11 -0800

    Body.jsx added

---
+++ frontend/src/pages/Body.jsx
@@ -0,0 +1,12 @@
+
+function Body() {
+    return <>
+        <div class="main-container">
+            <div class="body-container">
+                <h1>Welcome to scholarChips!</h1>
+            </div>
+        </div>
+    </>
+}
+
+export default Body;
