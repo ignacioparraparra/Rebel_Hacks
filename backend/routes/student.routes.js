248a7e6ca3b8253602d92187c05ada8311dd690b
Ignacio Parra <ignacioparra57@gmail.com>
2026-02-20 16:54:37 -0800

    ADDED SUPABASE INTEGRATION AND RETURN COINS

---
+++ backend/routes/student.routes.js
@@ -0,0 +1,45 @@
+require('dotenv').config()
+const express = require('express')
+const router = express.Router()
+
+router.use(express.json())
+
+const sql = require('../database.js')
+
+// RETURN CHIP COUNT
+router.post('/chips', async (req, res) => {
+    // GET STUDENTS CHIP COUNT FROM DB
+    const user_id = req.body.user_id
+    const chipCount = await getChipCount(user_id)
+    return res.send({chipCount})
+})
+
+// DEDUCT CHIPS 
+router.get('/chips', (req, res) => {
+    const chipDeduction = req.body.chipDeduction
+
+    // GET CHIP COUNT FROM DB 
+    // SUBTRACT CHIP COUNT FROM CHIP DEDUCTION
+
+    // RETURN SUCCESS 
+})
+
+async function getChipCount(student_id) {
+    const chipField = await sql `
+        SELECT COALESCE(SUM(amount), 0) AS total_chips
+        FROM chip_ledger
+        WHERE student_id=${student_id}`
+    const chipCount = chipField[0]["total_chips"]
+    return chipCount
+}
+
+async function getStudent(student_id) {
+    const name = await sql `
+        SELECT first_name
+        FROM students
+        WHERE id=${student_id}`
+    const first_name = name[0]['first_name']
+    return first_name
+}
+
+module.exports = router
--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,10 @@ const express = require('express')
 const app = express()
 
 const authRoutes = require('./routes/auth.routes.js')
-const studentRoutes = require('./routes/students.routes.js')
-const teacherRoutes = require('./routes.teachers.routes.js')
+const studentRoutes = require('./routes/student.routes.js')
+const teacherRoutes = require('./routes/teacher.routes.js')
+
+app.use(express.json())
 
 app.use('/auth', authRoutes)
 app.use('/student', studentRoutes)
